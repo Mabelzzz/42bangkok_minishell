f7c290e97dd7a95dcf8ae0770465bd48634c9132
Phatcharamon Namwayko <pnamwayk@z2t19c2.42bangkok.com>
2023-07-17 18:07:12 +0700

    yay

--- srcs/builtin/builtin_export.c
+++ srcs/builtin/builtin_export.c
@@ -9,31 +9,50 @@ int	builtin_export(t_main *main, t_cmd *cmd, char *str, int i)
 	char *first;
 
 	first = ft_strtrim(str, "=");
-	if (i < )
-	if (environ[i + 1])
-		compare = ft_strtrim(environ[i + 1], "=");
-	if (ft_strncmp(first, compare, ft_strlen(first)) < 0)
+	// if (i < )
+	// if (environ[i + 1])
+	// 	compare = ft_strtrim(environ[i + 1], "=");
+	// if (ft_strncmp(first, compare, ft_strlen(first)) < 0)
 
 	return (0);
 	// return (exit(0));
 }
 
-int print_sorted_env()
+int	sort_env(char **str)
 {
-	char *first;
-	int	i;=
+	int		i;
+	int		j;
 
 	i = -1;
-	first = ft_strtrim(str, "=");
-	while(environ[++i])
+	// str[i] = ft_strtrim(str, "=");
+	while (str[++i])
 	{
-		compare = ft_strtrim(environ[i + 1], "=");
-		if (ft_strncmp(first, compare, ft_strlen(first)) < 0)
-
+		j = -1;
+		while (str[++j])
+		{
+			if (ft_strncmp(str[i], str[j], ft_min_len(str[i], str[j])) > 0)
+				ft_swap(str[i], str[j]);
+		}
 	}
 }
 
+size_t	ft_len_trim(char *str, char trm)
+{
+	size_t	len;
 
+	len = 0;
+	while (str[len] != trm && str[len] != 0)
+		len++;
+	return (len);
+}
+
+size_t	ft_min_len(char *s1, char *s2)
+{
+	size_t	a;
+	size_t	b;
+
+
+}
 
 char	**ft_strdup2(char **str)
 {
@@ -79,3 +98,4 @@ void	ft_swap(char **s1, char **s2)
 	*s1 = *s2;
 	*s2 = tmp;
 }
+
